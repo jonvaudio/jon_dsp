11099ff0f04de7a39d93938197f7e7f0f039b650
Jon V <100223577+jonvaudio@users.noreply.github.com>
2022-07-03 14:16:48 +0100

    implemented dry wet mixer without smoothing

--- dry_wet_mixer.h
+++ dry_wet_mixer.h
@@ -21,6 +21,67 @@ inline VecType sg_vectorcall(dry_wet_mix)(const VecType dry, const VecType wet,
 // MIN_DB must be negative
 // If wet is set to MIN_DB, then it snaps to -inf dB (mul by zero)
 template <typename ParamType, int32_t MIN_DB>
+struct DryWetMixer_Instant {
+    struct ParamGroup : public ParamGroupManager<ParamGroup, ParamType> {
+        ParamValidated<ParamType> wet_lin, wet_lin_prev;
+    } param_;
+
+    static constexpr double min_db_ = static_cast<double>(MIN_DB);
+
+    void init() {
+        static_assert(MIN_DB < 0, "");
+        param_.init();
+    }
+
+    void reset() {
+        param_.wet_lin_prev.set(param_.wet_lin.get());
+    }
+
+    void set_wet(const ParamType wet) {
+        assert(((ParamType {0.0} <= wet) && (wet <= ParamType {1.0}))
+            .debug_valid_eq(true));
+        ParamType wet_lin = ((wet == 0.0) || (wet == 1.0))
+            .choose(wet, db_to_volt_std(min_db_ + (-min_db_ * wet)));
+        param_.wet_lin.set(wet_lin);
+    }
+
+    void set_wet_pc(const ParamType wet_pc) { set_wet(wet_pc * 0.01); }
+
+    bool any_dry_signal() {
+        static_assert(ParamType::elem_count == 1, "");
+        return param_.wet_lin.get().data() != 1.0;
+    }
+
+    bool any_wet_signal() {
+        static_assert(ParamType::elem_count == 1, "");
+        return param_.wet_lin.get().data() != 0.0;
+    }
+
+    // Call BEFORE iterate()
+    bool sg_vectorcall(should_reset_wet_before_iterate)() const {
+        static_assert(ParamType::elem_count == 1, "");
+        return param_.wet_lin_prev.get().data() == 0.0 &&
+            param_.wet_lin.get().data() != 0.0;
+    }
+
+    // Call BEFORE iterate()
+    bool sg_vectorcall(should_reset_dry_before_iterate)() const {
+        static_assert(ParamType::elem_count == 1, "");
+        return param_.wet_lin_prev.get().data() == 1.0 &&
+            param_.wet_lin.get().data() != 1.0;
+    }
+
+    template <typename VecType>
+    VecType iterate(const VecType dry, const VecType wet) {
+        reset();
+        const ArgType wet_lin = param_.wet_lin.get()
+            .template to<VecType>();
+        return dry_wet_mix(dry, wet, wet_lin);
+    }
+};
+
+// This one is smoothed
+template <typename ParamType, int32_t MIN_DB>
 struct DryWetMixer {
     struct SmoothParamGroup :
         public SmoothParamGroupManager<SmoothParamGroup, ParamType>
@@ -29,7 +90,7 @@ struct DryWetMixer {
     } param_;
 
     void sg_vectorcall(init)() {
-        assert(MIN_DB < 0);
+        static_assert(MIN_DB < 0, "");
         param_.init();
     }
 
